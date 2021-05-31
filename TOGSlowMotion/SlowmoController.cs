0cc97787d3f11edc5dd2742a98a5ea637f4af03a
Lil Husky VR <lilhuskyvr@gmail.com>
2021-05-31 20:36:45 +1000

    version 1.0

---
+++ TOGSlowMotion/SlowmoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace TOGSlowMotion
+{
+    public class SlowmoController : MonoBehaviour
+    {
+        public float lastClicked = 0;
+        public bool isSlowmo;
+        public float slowmoTimeScale = 0.5f;
+
+        private void Awake()
+        {
+            lastClicked = Time.time;
+        }
+    }
+}
---
+++ TOGSlowMotion/SlowmoPatch.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using HarmonyLib;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace TOGSlowMotion
+{
+    public class SlowmoPatch : MonoBehaviour
+    {
+        private Harmony _harmony;
+
+        public void Inject()
+        {
+            try
+            {
+                _harmony = new Harmony("SlowMotion");
+                _harmony.PatchAll(Assembly.GetExecutingAssembly());
+                Debug.Log("Slow Motion Loaded");
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(ex.Message);
+            }
+        }
+
+
+        [HarmonyPatch(typeof(VRCharController))]
+        [HarmonyPatch("Start")]
+        // ReSharper disable once UnusedType.Local
+        private static class VRCharControllerStartPatch
+        {
+            [HarmonyPostfix]
+            private static void Postfix(VRCharController __instance)
+            {
+                var slowmoController = __instance.gameObject.AddComponent<SlowmoController>();
+                var jsonInput = File.ReadAllText(Application.streamingAssetsPath +
+                                                 "/Mods/TOGSlowMotion/Settings.json");
+                
+                var data = JsonConvert.DeserializeObject<SlowmoControllerData>(jsonInput);
+                Debug.Log(data.slowmoTimeScale);
+                slowmoController.slowmoTimeScale = data.slowmoTimeScale;
+            }
+        }
+
+        [HarmonyPatch(typeof(VRCharController))]
+        [HarmonyPatch("LateUpdate")]
+        // ReSharper disable once UnusedType.Local
+        private static class VRCharControllerLateUpdatePatch
+        {
+            [HarmonyPostfix]
+            private static void Postfix(VRCharController __instance)
+            {
+                var slowmoController = __instance.gameObject.GetComponent<SlowmoController>();
+                if (slowmoController.isSlowmo && Mathf.Approximately(Time.timeScale, 1))
+                    Time.timeScale = slowmoController.slowmoTimeScale;
+
+                if (__instance.controlInput.OrderController.ButtonAPressed)
+                {
+                    if (Time.time - __instance.gameObject.GetComponent<SlowmoController>().lastClicked >= 0.5f)
+                    {
+                        Time.timeScale = Mathf.Approximately(Time.timeScale, 1) ? slowmoController.slowmoTimeScale : 1;
+
+                        slowmoController.lastClicked = Time.time;
+                        slowmoController.isSlowmo = !Mathf.Approximately(Time.timeScale, 1);
+                    }
+                }
+            }
+        }
+    }
+}
